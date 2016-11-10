d1644af829e537e676928cbac3e2c183f09ebf19
subrat71 <subratsarangi71@gmail.com>
2016-11-10 18:29:46 +0530

    - Updated Grunt setup

--- GruntFile.js
+++ GruntFile.js
@@ -10,17 +10,52 @@ module.exports = function(grunt) {
           base:'./',
           keepalive: true,
           open:{
-            target:'http://localhost:5050/index.html', // target url to open
+            target:'http://localhost:5050/ssapp/index.html', // target url to open
             appName:'Google Chrome', // name of the app that opens, ie: open, start, xdg-open
-            callback: function() {
-              grunt.log("Server connected !!!").ok();
+            callback: function(val) {
+            	console.log("val = ",val);
+              grunt.log(val," :: Server connected !!!").ok();
             } // called when the app has opened
           },         
           port: 5050
         }
       }
-    }        
-  })
+    },
+    copy: {
+    	main: {
+    		cwd: 'devapp',
+	        src: ['**','!devapp/**/*.css', '!devapp/**/*.js'],
+	        dest: 'ssapp',
+	        expand: true
+    	}
+	},
+	clean: {
+	  ssapp: {
+	    src: ['ssapp']
+	  }
+	},
+	watch: {
+		devapp: {
+			files: ['devapp/**/*.js','devapp/**/*.css','devapp/**/*.html'],
+      		tasks: ['devapp','notify:complete']
+		}
+	},	
+	notify: {
+	    task_name: {
+	      options: {
+	        // Task-specific options go here.
+	      }
+	    },
+	    complete: {
+	      options: {
+	        title: 'Compiled Successfully!!',  // optional
+	        message: 'Refresh your browser', //required
+	      }
+	    }
+	}
+  });
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-open');
   grunt.loadNpmTasks('grunt-http');
@@ -28,8 +63,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
 
-
-  grunt.registerTask('default', ['connect','open']);
-  grunt.registerTask('app', ['connect:server','open']);
-
+  grunt.registerTask('default', ['connect','open','watch']);
+  grunt.registerTask('app',['clean','copy','connect:server','open','watch']);
 };
